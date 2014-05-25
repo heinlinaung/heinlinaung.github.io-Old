696c6981dbbab6bff220ec0de7ffdc358f9f9906
heinlinaung <heinlinaung6891@gmail.com>
2014-05-25 19:45:41 +0630

    fixed for mouseover and mouseout event on CSS-NAV

--- assets/js/demo2.js
+++ assets/js/demo2.js
@@ -5,21 +5,24 @@
 
     //open and close menu when the button is clicked
 	var open = false;
-	button.addEventListener('click', handler, false);
+	// button.addEventListener('click', handler, false); // for MOBILE VERSION
+	button.addEventListener('mouseover', handler, false); 
+	wrapper.addEventListener('mouseout', handler, false);
 
 	function handler(){
 	  if(!open){
-	    this.innerHTML = " - ";
+	    button.innerHTML = " - ";
 	    classie.add(wrapper, 'opened-nav');
 	  }
 	  else{
-	    this.innerHTML = " + ";
+	    button.innerHTML = " + ";
 		classie.remove(wrapper, 'opened-nav');
 	  }
 	  open = !open;
 	}
-	function closeWrapper(){
-		classie.remove(wrapper, 'opened-nav');
-	}
+	// function closeWrapper(){
+	// 	classie.remove(wrapper, 'opened-nav');
+	// 	alert('closeWrapper')
+	// }
 
 })();
